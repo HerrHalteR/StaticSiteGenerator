9e7faa6e2bdcad4b3679a3bc6504802e67180a8a
HerrHalteR <herrhalter@gmail.com>
2026-01-23 20:55:05 +0100

    indent the fuck

--- src/leafnode.py
+++ src/leafnode.py
@@ -6,12 +6,12 @@ class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):    # tag & value have NO default values, making them REQUIRED. But 'props' still defaults to None as per the assignment
         super().__init__(tag, value, None, props)  # Since a "Leaf" cannot have children, we hardcode that as None when calling super().__init__.
         
-def to_html(self):
-    if self.value is None: # Your guard clause
-        raise ValueError("Invalid HTML: no value")
-    if self.tag is None:
-        return self.value
-    return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
-        
-    def __repr__(self):
-        return f"LeafNode(tag: {self.tag}, value: {self.value}, props: {self.props})"
+    def to_html(self):
+        if self.value is None: # Your guard clause
+            raise ValueError("Invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+            
+        def __repr__(self):
+            return f"LeafNode(tag: {self.tag}, value: {self.value}, props: {self.props})"

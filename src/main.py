f0490c14a63c7a24d895641a08af8318cffe1cae
HerrHalteR <herrhalter@gmail.com>
2026-01-23 19:01:58 +0100

    if name = main + correct enum type

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ from textnode import TextNode, TextType
 
 
 def main():
-    text = TextNode("Anchor text here", "some link", "https://www.boot.dev")
+    text = TextNode("Anchor text here", TextType.LINK, "https://www.boot.dev")
     print(text)
-    
+
+if __name__ == "__main__":
+    main()    
+

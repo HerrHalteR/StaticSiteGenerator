3a6aedef8da7dc6f9f876c0e88c06fc600f66d0b
HerrHalteR <herrhalter@gmail.com>
2026-01-26 02:51:09 +0100

    Debugging (forgot to import + extra "TEXT" somewh)

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,8 +1,11 @@
+from textnode import TextNode, TextType
+
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     
     for node in old_nodes:
-        if node.text_type != TextType.TEXT.TEXT:
+        if node.text_type != TextType.TEXT:
             new_nodes.append(node)
             continue
         

e7165579d6377c978067b70f5c3fe8191b4a0da7
HerrHalteR <herrhalter@gmail.com>
2026-01-27 03:10:03 +0100

    CH4 L3 incomplete

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -103,6 +103,6 @@ def text_to_textnodes(text):
     bold_split = split_nodes_delimiter(textnode_list, "**", TextType.BOLD)
     italic_split = split_nodes_delimiter(bold_split, "_", TextType.ITALIC)
     code_split = split_nodes_delimiter(italic_split, "`", TextType.CODE)
-    image_split =split_nodes_image(code_split)
+    image_split = split_nodes_image(code_split)
     link_split = split_nodes_link(image_split)
     return link_split

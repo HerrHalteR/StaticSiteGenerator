4a7db3bf2aa1de2bfb77d385ef6fdc67af033bca
HerrHalteR <herrhalter@gmail.com>
2026-01-30 01:42:14 +0100

    nooooode

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,8 +1,9 @@
 from enum import Enum
-
 from parentnode import ParentNode
 from inline_markdown import text_to_textnodes
-from textnode import text_node_to_html_node, TextNode, TextType
+from textnode import TextNode, TextType
+from textnode_to_htmlnode import text_node_to_html_node
+
 
 
 class BlockType(Enum):
